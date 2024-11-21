8d7cc1999ee12e17edefdc893b4b29184ad5a737
Deepan360 <deepandeep686@gmail.com>
2024-11-21 16:49:23 +0530

    app changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Import Router components
-import {
-  Box,
-  Typography,
-  Container,
-  Button,
-  Card,
-  CardContent,
-  CardActions,
-  Grid,
-} from "@mui/material";
-import drlogo from "/drcliniclogo.png";
-import Footer from "./components/footer"; // Correct relative path
+
 
 // Import the Login Page Components
 import DoctorLogin from "./pages/DoctorLogin";
@@ -21,9 +10,7 @@ import PharmaLogin from "./pages/PharmaLogin";
 import Home from "./pages/Home";
 
 function App() {
-  const handleNavigate = (path) => {
-    window.location.href = path; // Change to `useNavigate` if using React Router
-  };
+
 
   return (
     <Router>
