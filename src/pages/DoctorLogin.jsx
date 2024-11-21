98072c789c17b46ddf15df356a5cb35c6d2e70b2
Deepan360 <deepandeep686@gmail.com>
2024-11-21 17:59:53 +0530

    end of day

--- src/pages/DoctorLogin.jsx
+++ src/pages/DoctorLogin.jsx
@@ -2,18 +2,35 @@
 import React from "react";
 import { Box, Typography, Container, TextField, Button } from "@mui/material";
 
+
 function DoctorLogin() {
   return (
-    <Container>
-      <Box sx={{ padding: 3, textAlign: "center" }}>
-        <Typography variant="h4" gutterBottom>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh", // Ensures the full viewport height is use
+      }}
+    >
+      <Container
+        sx={{
+          background: "#f1f1f1",
+          borderRadius: "15px",
+          component: "main",
+          padding: "20px",
+          coloredShadow: "info",
+          textAlign: "center",
+        }}
+      >
+        <Typography variant="h4" sx={{color: "#0d8279"}} gutterBottom>
           Doctor Login
         </Typography>
         <TextField
           label="Username"
           variant="outlined"
           fullWidth
-          sx={{ marginBottom: 2 }}
+          sx={{ marginBottom: 2, color: "#0d8279" }}
         />
         <TextField
           label="Password"
@@ -22,11 +39,11 @@ function DoctorLogin() {
           fullWidth
           sx={{ marginBottom: 2 }}
         />
-        <Button variant="contained" color="primary">
+        <Button variant="contained" color="info"    fullWidth>
           Login
         </Button>
-      </Box>
-    </Container>
+      </Container>
+    </Box>
   );
 }
 
